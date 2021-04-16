d7a84a733baf465297be42c02062f67a365e1727
matwieclawek <mateusz.wieclawek@outlook.com>
2021-04-16 13:43:04 +0200

    init

---
+++ Domowabiblioteka/app/src/main/java/com/example/domowabiblioteka/BookInfo.kt
@@ -0,0 +1,12 @@
+package com.example.domowabiblioteka
+
+class BookInfo(title:String,subtitle:String, authors: ArrayList<String>,publisher:String,publishedDate:String,description:String, pages:Int ) {
+    private lateinit var title:String
+    private lateinit var subtitle:String
+    private lateinit var authors: ArrayList<String>
+    private lateinit var publisher: String
+    private lateinit var publishedDate:String
+    private lateinit var descprition:String
+    private var pages=0
+
+}
